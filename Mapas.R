ca1e9e7e2ea2f4fd1828e3f1bfd6115b057c704b
unknown <externo@M-tecnica-014.calymayor.com.mx>
2021-04-06 23:42:23 -0500

    Update 04/06/2021

--- Mapas.R
+++ Mapas.R
@@ -1,16 +1,34 @@
 #Map
-title = "Mapa de Estres Hídrico de México"
-capt = "Fuente: WRI Aqueduct ranking, 2019"
+title = "Mapa de Estrés Hídrico de México"
+capt = "Fuente: WRI Aqueduct 2019"
 gg<-df %>%
   ggplot()+
-  geom_sf(aes(fill=score))+
+  geom_sf(aes(fill=cat_estres))+
   labs(title=title, caption = capt)+
-  scale_fill_viridis_c(option = "inferno", direction=-1,alpha = 0.8, name = "Nivel de estres",
-                       breaks = c(1, 2, 3,4),
+  scale_fill_viridis_c(option = "inferno", direction=-1,alpha = 0.8, name = "Nivel de estrés",
+                       breaks = c(1,2,3,4),
                        labels = c("Bajo","Medio","Alto", "Extremo Alto"))+
   theme(panel.grid.minor.x=element_blank(), panel.grid.major.x=element_blank(),
         panel.background = element_blank(),axis.title.x=element_blank(),
         axis.text.x=element_blank(),
-        axis.ticks.x=element_blank())
+        axis.ticks.x=element_blank(),axis.ticks.y=element_blank(),
+        axis.text.y=element_blank())
 
 gg
+
+title2 = "Mapa de Riesgo de Sequia en México"
+capt = "Fuente: WRI Aqueduct 2019"
+gg2<-df %>%
+  ggplot()+
+  geom_sf(aes(fill=cat_ddr))+
+  labs(title=title2, caption = capt)+
+  scale_fill_viridis_c(option = "inferno", direction=-1,alpha = 0.8, name = "Riesgo",
+                       breaks = c(0,1,2,3),
+                       labels = c("Bajo","Bajo-Medio","Medio", "Medio-Alto"))+
+  theme(panel.grid.minor.x=element_blank(), panel.grid.major.x=element_blank(),
+        panel.background = element_blank(),axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank(),axis.ticks.y=element_blank(),
+        axis.text.y=element_blank())
+
+gg2
---
+++ data/README.md
@@ -0,0 +1,20 @@
+# MEET UP: CREACIÓN DE MAPAS
+
+El objetivo de la sesión es introducir a las disntintas metodologías para la elaboración de mapas con distintas packages.
+
+
+![Optional Text](https://pbs.twimg.com/media/Eu9jC09UUAIMIGd?format=jpg&name=small)
+
+#### Package requerida
+```
+install.packages(c("devtools", "ggplot2", "maps", "dplyr", "rgdal", "sf", "raster"
+                 "plotly", "scales", "viridis", "leaflet", "proj4")
+
+
+if (!require("devtools")) {
+          install.packages("devtools", force=TRUE)
+        }
+        devtools::install_github("diegovalle/mxmaps")
+
+```
+
