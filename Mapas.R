bbf60eb1b6a728c022cb50d46c38f53b58561ec8
unknown <externo@M-tecnica-014.calymayor.com.mx>
2021-04-07 23:27:41 -0500

    update 04/07/2021

--- Mapas.R
+++ Mapas.R
@@ -1,34 +1,58 @@
 #Map
-title = "Mapa de Estrés Hídrico de México"
+title = "Colima y el agua"
+subt = "Mapa de Estrés Hídrico"
 capt = "Fuente: WRI Aqueduct 2019"
 gg<-df %>%
   ggplot()+
-  geom_sf(aes(fill=cat_estres))+
-  labs(title=title, caption = capt)+
+  geom_sf(aes(fill=score_estres), colour = NA)+
+  labs(title=title,subtitle = subt, caption = capt)+
   scale_fill_viridis_c(option = "inferno", direction=-1,alpha = 0.8, name = "Nivel de estrés",
                        breaks = c(1,2,3,4),
-                       labels = c("Bajo","Medio","Alto", "Extremo Alto"))+
-  theme(panel.grid.minor.x=element_blank(), panel.grid.major.x=element_blank(),
-        panel.background = element_blank(),axis.title.x=element_blank(),
-        axis.text.x=element_blank(),
-        axis.ticks.x=element_blank(),axis.ticks.y=element_blank(),
-        axis.text.y=element_blank())
+                       labels = c("Bajo","Medio","Alto", "Extremo Alto"),
+                       guide = guide_legend(
+                         direction = 'horizontal',
+                         title.position = 'top',
+                         title.hjust = .5,
+                         label.hjust = .5,
+                         label.position = 'bottom',
+                         keywidth = 3,
+                         keyheight = .5
+                       ))+
+  theme_void()+
+  theme(title=element_text(face='bold'),
+        legend.position = 'bottom')
 
 gg
 
-title2 = "Mapa de Riesgo de Sequia en México"
+subt2 = "Mapa de Riesgo de Sequia en México"
 capt = "Fuente: WRI Aqueduct 2019"
 gg2<-df %>%
   ggplot()+
-  geom_sf(aes(fill=cat_ddr))+
-  labs(title=title2, caption = capt)+
+  geom_sf(aes(fill=score_ddr), colour=NA)+
+  labs(title=title, subtitle = subt2, caption = capt)+
   scale_fill_viridis_c(option = "inferno", direction=-1,alpha = 0.8, name = "Riesgo",
-                       breaks = c(0,1,2,3),
-                       labels = c("Bajo","Bajo-Medio","Medio", "Medio-Alto"))+
-  theme(panel.grid.minor.x=element_blank(), panel.grid.major.x=element_blank(),
-        panel.background = element_blank(),axis.title.x=element_blank(),
-        axis.text.x=element_blank(),
-        axis.ticks.x=element_blank(),axis.ticks.y=element_blank(),
-        axis.text.y=element_blank())
+                       breaks = c(0.2,0.4,0.59,0.7),
+                       labels = c("Bajo","Bajo-Medio","Medio", "Medio-Alto"),
+                       guide = guide_legend(
+                         direction = 'horizontal',
+                         title.position = 'top',
+                         title.hjust = .5,
+                         label.hjust = .5,
+                         label.position = 'bottom',
+                         keywidth = 3,
+                         keyheight = .5
+                       ))+
+  theme_void()+
+  theme(title=element_text(face='bold'),
+        legend.position = 'bottom')
+  
+  
+
 
 gg2
+
+theme(panel.grid.minor.x=element_blank(), panel.grid.major.x=element_blank(),
+      panel.background = element_blank(),axis.title.x=element_blank(),
+      axis.text.x=element_blank(),
+      axis.ticks.x=element_blank(),axis.ticks.y=element_blank(),
+      axis.text.y=element_blank())
